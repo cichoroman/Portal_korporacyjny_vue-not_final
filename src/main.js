b955b59e892b729086205573d15b0dc16fefe8b9
cichoroman <35227357+cichoroman@users.noreply.github.com>
2019-02-04 15:25:19 +0100

    vue resource

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import BootstrapVue from "bootstrap-vue"
 import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
-
+import VueResource form "vue-resource"
 Vue.use(BootstrapVue)
+
+Vue.use(VueResorce);
+
+
 
 new Vue({
   el: '#app',
